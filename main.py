8dc70955553fb19f7ce0f846f7584f4d31639876
Justus Peterson-Rhodes <justus.rho4@gmail.com>
2019-04-16 12:04:46 -0600

    Cleaned up new post form, added error messages for blog posts.

--- main.py
+++ main.py
@@ -22,21 +22,43 @@ class Blog(db.Model):
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
   
-
   blogs = Blog.query.all()
   return render_template("blog.html", title="Build-A-Blog", blogs=blogs)
 
-@app.route('/newpost', methods=['POST', 'GET'])
-def add_post():
+@app.route('/newpost', methods=['GET'])
+def render_form():
 
-  if request.method == 'POST':
-    post_title = request.form['title']
-    post_body = request.form['body']
+  return render_template("add-post.html", title="Add Post")
+
+@app.route('/newpost', methods=['POST'])
+def add_post():
+  post_title = request.form['title']
+  post_body = request.form['body']
+  title_error = ''
+  body_error = ''
+
+  # Check if title is empty
+  if not post_title:
+    title_error = "Please enter a post title"
+  
+  # Check if body is empty
+  if not post_body:
+    body_error = "Please enter body text for your post"
+
+  # If there's any errors, rerender the form with the messages
+  if title_error or body_error:
+    return render_template("add-post.html", title="Add Post", 
+    title_error=title_error, body_error=body_error, 
+    post_title=post_title, post_body=post_body)
+
+  # If there's no errors, good to add the post
+  else:
     new_post = Blog(post_title, post_body)
     db.session.add(new_post)
     db.session.commit()
+    return redirect("/blog")
 
-  return render_template("add-post.html", title="Add Post")
+  
 
 if __name__ == "__main__":
   app.run()
