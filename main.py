ae77018ddf28919c0157e724d587cc4c3bc6aea3
Justus <justus_pete@hotmail.com>
2019-04-18 20:01:43 -0500

    Added user class

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 # App Initialization
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://build-a-blog:420Hornze!@localhost:8889/build-a-blog"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://blogz:420Hornze!@localhost:8889/blogz"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -16,6 +16,7 @@ class Blog(db.Model):
   title = db.Column(db.String(80))
   body = db.Column(db.String(300))
   post_time = db.Column(db.DateTime)
+  owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
   def __init__(self, title, body, post_time=None):
     self.title = title
@@ -23,6 +24,17 @@ class Blog(db.Model):
       post_time = datetime.utcnow()
     self.post_time = post_time
     self.body = body
+
+# User Class
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80))
+  password = db.Column(db.String(30))
+  blogs = db.relationship('Blog', backref='owner')
+
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
 
 # Main Blog page
 @app.route('/blog', methods=['POST', 'GET'])
